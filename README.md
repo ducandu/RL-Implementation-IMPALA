2a9c44e4f30ff98ecd42302c4fc413c0819e5c77
sven1977 <svenmika1977@gmail.com>
2018-03-15 13:34:26 +0100

    - first working algo (reaches optimal solution with one learner, one explorer) - added more documentation

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # RL-implement-IMPALA
 A Test-Implementation of the IMPALA algorithm (by deepmind 2018)
 
-**IMPORTANT NOTE: Currently, V-trace is not implemented! We are using simple REINFORCE by
-Williams 1992 here.**
+![Grid World Barto/Sutton new 2017 draft Chapter 13.1](https://raw.githubusercontent.com/ducandu/RL-Implementation-IMPALA/master/grid_world.png "The grid world we are trying to solve")
+
+**IMPORTANT NOTE: Currently, IMPALA's "V-trace"-correction is not
+implemented! Instead, we are using simple REINFORCE with a state-value
+function baseline by Williams (1992).**
 
 The point of this repo is to demonstrate how the distributed tensorflow part
 described in the paper can be implemented quite easily using two job types
-(explorers and learners).
+('explorers' and 'learners').
 
 See the following paper for more details:
 
@@ -23,23 +26,24 @@ https://arxiv.org/abs/1802.01561
 - Because exploring (and the collected experiences) is off-policy, they introduce a trick - v-traces -
   to adjust the vanilla PG update for this off-policy case.
 
+A couple of scripts should be run in parallel to simulate the distributed setup.
 Run:
 
 ```
-python impala_proto.py -h
+$ python impala_proto.py -h
 ```
 
 to see possible command line options.
 
+To run a most basic actual RL-job, start the following two scripts in parallel:
+
 ```
-python impala_proto.py -l localhost:22222 -e localhost:22223  -j explorer -t 0
-
-And parallel:
-
-python impala_proto.py -l localhost:22222 -e localhost:22223  -j learner -t 0
+$ python impala_proto.py -l localhost:22222 -e localhost:22223 -j learner -t 0
+$ python impala_proto.py -l localhost:22222 -e localhost:22223 -j explorer -t 0
 ```
 
-Will run the algo on the local machine using one learner and one explorer (agent).
+This will run the algo on the local machine using one learner and one explorer (agent).
+Add more learners or explorers if you like.
 The built-in Env is the one taken from *Barto & Sutton*:
 **Reinforcement Learning: An Introduction** "2017 Completed Draft" Chapter 13
 (Policy Gradient Methods) Fig 13.1.
@@ -50,8 +54,14 @@ and 4 states. The optimal policy is to move right with a probability of
 
 The learner process outputs the loss and the average probability of moving right
 for the simple policy/state-value network used and this probability should
-converge to 0.59 in the long run. The neural net used is a simple two-layer
+converge to 0.59 after a few 10k training steps depending on weight initialization,
+learning_rate (default is 0.001), and - yes - many many other factors :)
+
+```
+task 0 step 45910 loss 10.399235725402832 avg-right-prob=0.5933351516723633
+```
+*Output of the "learner" job, task 0 around the point where it found the optimal policy:*
+
+The neural net used is a simple two-layer
 dense network with 1 input for the state (always 1.0), n hidden nodes (default 10),
 and 3 output nodes (2 action probs (left and right), 1 state-value).
-
-
--- impala_proto.py
+++ impala_proto.py
@@ -32,24 +32,21 @@ from test_discounted_return import discount
 # - Uses n central learners (which are function as parameter servers) and
 #   m explorer agents that collect experience from a simple Env taken from Barto&Sutton "completed 2017 draft"
 #   Chapter 13 (policy gradient methods).
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.register("type", "bool", lambda v: v.lower() == "true")
-
     parser.add_argument("-l", "--learner-hosts", type=str, required=True,
                         help="Comma-separated list of hostname:port pairs")
     parser.add_argument("-e", "--explorer-hosts", type=str, required=True,
                         help="Comma-separated list of hostname:port pairs")
     parser.add_argument("-j", "--job-name", type=str, default="worker", help="One of 'learner', 'explorer'")
     parser.add_argument("-t", "--task-index", type=int, default=0, help="Index of task within the job")
-    parser.add_argument("-s", "--steps-per-explorer", type=int, default=10000,
+    parser.add_argument("-s", "--steps-per-explorer", type=int, default=1000000,
                         help="Max. steps an explorer should make in its env (in total before stopping).")
     parser.add_argument("-m", "--max-steps-per-episode", type=int, default=100,
                         help="Max. steps an explorer should make in one episode.")
-    parser.add_argument("-b", "--buffer-size", type=int, default=100,
+    parser.add_argument("-b", "--buffer-size", type=int, default=1000,
                         help="Number of time steps to store at any time for each explorer.")
-    parser.add_argument("--learn-batch-size", type=int, default=32,
+    parser.add_argument("--learn-batch-size", type=int, default=64,
                         help="Size of a batch (number of episodes) to pull randomly from the main buffer "
                              "for each learner iteration.")
     parser.add_argument("-f", "--upload-frequency", type=int, default=4,
@@ -57,11 +54,10 @@ def main():
                              "to the learners?")
     parser.add_argument("--num-hidden", type=int, default=10,
                         help="Number of hidden nodes.")
-    parser.add_argument("-g", "--gamma", type=float, default=0.9,
+    parser.add_argument("-g", "--gamma", type=float, default=0.97,
                         help="The discount factor gamma (default 0.9).")
-    parser.add_argument("-a", "--learning-rate", type=float, default=0.0001,
+    parser.add_argument("-a", "--learning-rate", type=float, default=0.001,
                         help="The learning rate (alpha) to use for optimizing the cost.")
-
     args = parser.parse_args()
     learner_hosts = args.learner_hosts.split(",")
     explorer_hosts = args.explorer_hosts.split(",")
@@ -76,7 +72,8 @@ def main():
     num_inputs = 1  # input is always [1.0] -> blind env
     num_hidden = args.num_hidden
     num_out = 2 + 1  # left and right actions + value function
-    len_buffer_record = 3  # 1 disc. return, 1 log(action-right-probability) (from network), 1 value (from network)
+    # 1 disc. return, 1 action (0=left, 1=right),
+    len_buffer_record = 2
 
     # the global main policy: pi (live on the learner(s))
     # - explorers sync their own policies (mu) with this at the beginning of an episode
@@ -100,9 +97,6 @@ def main():
         global_step = tf.train.get_or_create_global_step()
 
     if args.job_name == "explorer":
-        # number of actions to sample (usually 1 as we are doing step-by-step exploration)
-        num_action_samples = tf.placeholder(dtype=tf.int32, shape=())
-
         # local policy (mu) -> all zero; will be sync'd with pi anyway at start of each episode
         weights_1_mu = tf.Variable(tf.zeros(shape=(num_inputs, num_hidden)), name="mu-W1")
         biases_1_mu = tf.Variable(tf.zeros(shape=(num_hidden,)), name="mu-b1")
@@ -117,7 +111,8 @@ def main():
             tf.assign(weights_2_mu, weights_2_pi, name="sync-W2"),
             tf.assign(biases_2_mu, biases_2_pi, name="sync-b2")
         ]
-
+        # number of actions (log-action-probs) to sample
+        num_action_samples = tf.placeholder(dtype=tf.int32, shape=())
         # Buffer to store n (capacity) episodes of experiences (round-robin).
         # This one gets inserted into the learners global memory after each m (upload-frequency) episodes.
         # rank0=episode, rank1=step in episode, rank2=[action(0=left, 1=right), reward, mu(a)]
@@ -137,21 +132,29 @@ def main():
         hidden_out = tf.add(tf.matmul(tf.ones(shape=(num_action_samples, num_inputs)), weights_1_mu), biases_1_mu)
         logits = tf.add(tf.matmul(hidden_out, weights_2_mu), biases_2_mu)
         action_prob = tf.nn.softmax(logits[:, :2])  # first two outputs are action logits
-        values = logits[:, 2]  # last output is the state-value
-        log_action_prob = tf.log(action_prob)
         action_distr = tf.distributions.Bernoulli(probs=action_prob[:, 1])
         actions = action_distr.sample(sample_shape=num_action_samples)
 
-        # store incoming step (R, log(a), V) in local experience buffer
+        # store incoming step (a, R) in local experience buffer
+        a_in = tf.placeholder(dtype=tf.float32, shape=(None,), name="a-in")  # 0.0=left, 1.0=right
         returns_in = tf.placeholder(dtype=tf.float32, shape=(None,), name="returns-in")  # None=timesteps in the episode
-        log_ap_in = tf.placeholder(dtype=tf.float32, shape=(None,), name="log-a-prob-in")
-        v_in = tf.placeholder(dtype=tf.float32, shape=(None,), name="V-in")
         # concat returns and log_aps within each timestep
-        episode = tf.concat([tf.expand_dims(returns_in, 1), tf.expand_dims(log_ap_in, 1), tf.expand_dims(v_in, 1)], 1)
-        episode_len = tf.shape(episode)[0] + 1
+        episode = tf.concat([tf.expand_dims(a_in, 1), tf.expand_dims(returns_in, 1)], 1)
+        episode_len = tf.shape(episode)[0]
+        stop = experience_buffer_idx + episode_len
         # don't have to lock as the only one that's ever touching the local buffer is ourselves
-        add_episode = tf.assign(experience_buffer[experience_buffer_idx:experience_buffer_idx+episode_len],
-                                episode, use_locking=False)
+        add_episode = tf.cond(stop <= args.buffer_size,
+                              # true fn
+                              lambda: tf.group(tf.assign(experience_buffer[experience_buffer_idx:stop],
+                                                         episode, use_locking=False)),
+                              # false fn
+                              lambda: tf.group(
+                                  tf.assign(experience_buffer[experience_buffer_idx:],
+                                            episode[:args.buffer_size-experience_buffer_idx], use_locking=False),
+                                  tf.assign(experience_buffer[:episode_len-(args.buffer_size-experience_buffer_idx)],
+                                            episode[args.buffer_size-experience_buffer_idx:], use_locking=False)
+                              )
+                              )
 
         # create our own private env
         env = Env()
@@ -165,10 +168,9 @@ def main():
         with tf.train.MonitoredTrainingSession(master=server.target,
                                                is_chief=(args.task_index == 0),
                                                hooks=[]) as mon_sess:
-            while not total_steps > args.steps_per_explorer:
+            while total_steps < args.steps_per_explorer:
                 rs = []  # discounted accum. rewards over one episode
-                log_aps = []  # log action probabilities over one episode
-                vs = []  # the value outputs produced  by the network
+                as_ = []  # the actual actions taken
                 # reset the env
                 env.reset()
                 episode_steps = 0
@@ -178,66 +180,67 @@ def main():
 
                 while episode_steps < args.max_steps_per_episode and total_steps < args.steps_per_explorer:
                     s = env.state
-                    a, log_ap, v = mon_sess.run([actions, log_action_prob, values], feed_dict={num_action_samples: 1})
+                    a = mon_sess.run(actions, feed_dict={num_action_samples: 1})
                     a = a[0][0]  # a=0 for 'left', a=1 for 'right'
-                    log_ap = log_ap[0][a]  # probability of picking a
-                    v = v[0]
-                    log_aps.append(log_ap)
-                    vs.append(v)
+                    as_.append(a)
                     r, is_terminal = env.execute(a)
                     rs.append(r)
 
-                    print("{:03d} explorer {}: s={} action {} (1=right) s'={} is-term={} v-out={}".
-                          format(total_steps, args.task_index, s, a, env.state, is_terminal, v))
+                    print("{:03d} explorer {}: s={} action {} (1=right) s'={} is-term={}".
+                          format(total_steps, args.task_index, s, a, env.state, is_terminal))
                     total_steps += 1
                     episode_steps += 1
 
                     if is_terminal:
-                        # calculate discounted accum rewards (returns)
+                        # calculate discounted accumulated rewards (returns)
                         returns = discount(rs, args.gamma)
 
                         # add episode to our buffer
-                        mon_sess.run([add_episode], feed_dict={returns_in: returns, log_ap_in: log_aps,
-                                                               v_in: vs,
-                                                               experience_buffer_idx: buffer_idx})
-
+                        mon_sess.run([add_episode],
+                                     feed_dict={returns_in: returns, a_in: as_, experience_buffer_idx: buffer_idx})
                         env.reset()
+                        buffer_idx = (buffer_idx + len(rs)) % args.buffer_size
                         rs = []
-                        log_aps = []
-                        vs = []
+                        as_ = []
                         episode_steps = 0
-                        buffer_idx = (buffer_idx + 1) % args.buffer_size
                         num_episodes += 1
                         fetches = sync_ops
                         if num_episodes % args.upload_frequency == 0:
                             fetches.append(experience_upload)
                         fetches = mon_sess.run(fetches)
+        print("Explorer {} is done!".format(args.task_index))
 
     # - every learner iteration, it samples randomly from the main buffer and learns
     else:
-        # for now: do simple REINFORCE (add v-trace later or directly to tensorforce as it's not really different)
+        # build the pi-network (similar to mu-network above)
+        hidden_out = tf.add(tf.matmul(tf.ones(shape=(args.learn_batch_size, num_inputs)), weights_1_pi), biases_1_pi)
+        logits = tf.add(tf.matmul(hidden_out, weights_2_pi), biases_2_pi)
+        action_right_prob = tf.nn.softmax(logits[:, :2])[:, 1]  # first two outputs are action logits (1=right)
+        avg_action_right_prob = tf.reduce_mean(action_right_prob)
+        values = logits[:, 2:3]  # last output is the state-value
 
+        # for now: do simple REINFORCE (add v-trace later or directly to tensorforce as it's not really different)
         # get a random batch from the main buffer
         indexes = tf.random_shuffle(tf.range(size_main_experience_buffer))
-        # sample is a rank3 tensor with rank0=batch (episode), rank1=timestep,
-        # rank2=[accum return, log-action-right-prob]
         sample = tf.gather(main_experience_buffer, indexes[:args.learn_batch_size])
-        # separate discounted accum. reward and log-action-right-prob
-        [returns, log_aps, values] = tf.split(sample, num_or_size_splits=3, axis=1)
+        # separate discounted accum. reward and action (0=left, 1=right)
+        actions, returns = tf.split(sample, num_or_size_splits=len_buffer_record, axis=1)
+        # probability of the action actually taken
+        action_prob = tf.abs(tf.subtract(actions, tf.ones(tf.shape(actions))) + action_right_prob)
+        log_action_prob = tf.log(action_prob)
 
         # define our loss function
         alpha_1 = 0.5  # log-action-prob loss
         alpha_2 = 0.4  # value function loss
         alpha_3 = 0.1  # regularization
-        # TODO: add regularization
-        # reduce-mean on batch and timestep axes
-        avg_log_right_prob = tf.reduce_mean(log_aps, (0, 1))
-        loss = - alpha_1 * avg_log_right_prob + \
-               alpha_2 * tf.reduce_mean(0.5 * tf.square(returns - values), (0, 1)) + \
-               alpha_3 * 0
-        avg_right_prob = tf.exp(avg_log_right_prob)
 
-        train_op = tf.train.AdagradOptimizer(0.001).minimize(loss, global_step=global_step)
+        # reduce-mean on batch and timestep axes
+        advantage = (returns - values)
+        loss = - alpha_1 * tf.reduce_mean(tf.multiply(log_action_prob, advantage)) + \
+               alpha_2 * tf.reduce_mean(0.5 * tf.square(advantage)) + \
+               alpha_3 * 0  # TODO: add regularization
+
+        train_op = tf.train.AdagradOptimizer(args.learning_rate).minimize(loss, global_step=global_step)
 
         # The MonitoredTrainingSession takes care of session initialization,
         # restoring from a checkpoint, saving to a checkpoint, and closing when done
@@ -246,9 +249,12 @@ def main():
                                                is_chief=(args.task_index == 0),
                                                hooks=[]) as mon_sess:
             while not mon_sess.should_stop():
-                _, loss_out, g_step, avg_prob_right_out = mon_sess.run([train_op, loss, global_step, avg_right_prob])
+                sample_out, _, loss_out, g_step, avg_right_prob_out = mon_sess.run(
+                    [sample, train_op, loss, global_step, avg_action_right_prob]
+                )
                 print("task {} step {} loss {} avg-right-prob={}".
-                      format(args.task_index, g_step, loss_out, avg_prob_right_out))
+                      format(args.task_index, g_step, loss_out, avg_right_prob_out))
+        print("Learner {} is done!".format(args.task_index))
 
 
 if __name__ == "__main__":
